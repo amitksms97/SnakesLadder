97a5d6386ddfb17fa6818a5edfb3692e2daefd98
amtksms97 <amtksms97@gmail.com>
2020-11-27 10:11:26 +0530

    First Commit

---
+++ src/SnakesLadder.java
@@ -0,0 +1,14 @@
+public class SnakesLadder {
+
+	static int position;
+	public SnakesLadder() {
+		System.out.println("Welcome to Snake and Ladder Game");
+		position=0;
+	}
+	public static void main(String[] args) {
+		//SnakesLadder obj= new SnakesLadder();
+		System.out.println(position);
+
+	}
+
+}
