3dfdd81953d9b40548d788395c13055997fb4c1e
amtksms97 <amtksms97@gmail.com>
2020-11-27 10:29:15 +0530

    "Added code to get a random move from the player"

--- src/SnakesLadder.java
+++ src/SnakesLadder.java
@@ -1,13 +1,23 @@
+import java.util.Random;
 public class SnakesLadder {
 
+	static int roll;
 	static int position;
 	public SnakesLadder() {
 		System.out.println("Welcome to Snake and Ladder Game");
 		position=0;
 	}
+	public void move()
+	{
+		Random rand=new Random();
+		roll=rand.nextInt(6)+1;
+	}
 	public static void main(String[] args) {
 		SnakesLadder obj= new SnakesLadder();
-		System.out.println(position);
+		System.out.println("Initial Position: "+position);
+		obj.move();
+		System.out.println(roll);
+		
 
 	}
 
