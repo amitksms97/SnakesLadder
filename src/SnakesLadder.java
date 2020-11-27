efb3be851e79be7f19c030536cb79e4380279b35
amtksms97 <amtksms97@gmail.com>
2020-11-27 11:10:42 +0530

    "Added code to find the position of user after rolling the dice"

--- src/SnakesLadder.java
+++ src/SnakesLadder.java
@@ -1,22 +1,47 @@
 import java.util.Random;
+import java.util.*;
 public class SnakesLadder {
 
-	static int roll;
+	Scanner sc = new Scanner(System.in);
+	Random rand=new Random();
+	static int rollDice;
 	static int position;
 	public SnakesLadder() {
 		System.out.println("Welcome to Snake and Ladder Game");
 		position=0;
 	}
-	public void move()
+	public void rollingDice()
 	{
-		Random rand=new Random();
-		roll=rand.nextInt(6)+1;
+		rollDice=rand.nextInt(6)+1;
+	}
+	public void options()
+	{
+		int option=rand.nextInt(3);
+		switch (option) {
+		case 0: 
+			System.out.println("No Play");
+			position=0;
+		break;
+		case 1:
+			System.out.println("Ladder");
+			position=position+rollDice;
+		break;
+		case 2:
+			System.out.println("Snakes");
+			position=position-rollDice;
+			if(position<0)
+			position=0;
+			break;
+		default:
+			System.out.println("Invalid Input");
+		}
 	}
 	public static void main(String[] args) {
 		SnakesLadder obj= new SnakesLadder();
 		System.out.println("Initial Position: "+position);
-		obj.move();
-		System.out.println(roll);
+		obj.rollingDice();
+		obj.options();
+		System.out.println("New Position: "+position);
 		
 
 	}
